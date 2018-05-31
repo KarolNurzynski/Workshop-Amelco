313d92fbe1ff7848186644ec1587e6eeabb857d0
KarolNurzynski <karolnurzynski@gmail.com>
2018-05-31 23:21:46 +0200

    Minor changes to Event entity

--- src/main/java/pl/coderslab/entity/Event.java
+++ src/main/java/pl/coderslab/entity/Event.java
@@ -20,7 +20,7 @@ public class Event {
 
     private String name;
 
-    private LocalDateTime startDateTime;
+    private LocalDateTime localDateTime;
 
     @ManyToOne
     @JoinColumn(name = "sport_id")
